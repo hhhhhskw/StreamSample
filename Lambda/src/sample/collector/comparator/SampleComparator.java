c21b3d9917660979deb9a5ba70b95ba18d5e5b1d
hhhhhskw <y_10w40_h@yahoo.co.jp>
2015-02-12 23:48:56 +0900

    Revert "Revert "first commit""
    This reverts commit 964284be15d19d99a7d47e638d55382ae288fbd9.

---
+++ Lambda/src/sample/collector/comparator/SampleComparator.java
@@ -0,0 +1,13 @@
+package sample.collector.comparator;
+
+import java.util.Comparator;
+
+public class SampleComparator implements Comparator<Integer>{
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
